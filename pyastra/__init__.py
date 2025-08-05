f33179ed5c2369e274430c04e29b251c9a9fceac
andrei1345 <72022571+andrei1345@users.noreply.github.com>
2025-08-05 14:21:15 +0300

    Update __init__.py

--- pyastra/__init__.py
+++ pyastra/__init__.py
@@ -2,26 +2,6 @@
 # $Id: __init__.py 20 2004-04-21 15:15:48Z estyler $
 #
 # Description: pyastra modules. Pyastra project.
-# Author: Alex Ziranov <estyler _at_ users _dot_ sourceforge _dot_ net>
-#    
-# Copyright (c) 2004 Alex Ziranov.  All rights reserved.
-#
-############################################################################
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-#
-############################################################################
-# Author of original Pyastra
+# Author of original Pyastra: Alex Ziranov <estyler _at_ users _dot_ sourceforge _dot_ net>. See information at https://pyastra.sourceforge.net/
+##########################################################################
 __all__ = ['ports']
