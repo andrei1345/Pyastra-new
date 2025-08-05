fe40a1546f69ce8092a0a0d9cc73b03705f0a967
andrei1345 <72022571+andrei1345@users.noreply.github.com>
2025-08-05 15:56:41 +0300

    Update __init__.py

--- pyastra/__init__.py
+++ pyastra/__init__.py
@@ -2,6 +2,6 @@
 # $Id: __init__.py 20 2004-04-21 15:15:48Z estyler $
 #
 # Description: pyastra modules. Pyastra project.
-# Author of original Pyastra: Alex Ziranov <estyler _at_ users _dot_ sourceforge _dot_ net>. See information at https://pyastra.sourceforge.net/
+# Author of original Pyastra: Alex Ziranov <estyler _at_ users _dot_ sourceforge _dot_ net>. See more information at https://pyastra.sourceforge.net/
 ##########################################################################
 __all__ = ['ports']
