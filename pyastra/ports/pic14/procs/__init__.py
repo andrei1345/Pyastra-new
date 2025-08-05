8b4e4b7221034d0c3513423e0b6b4011d8717187
andrei1345 <72022571+andrei1345@users.noreply.github.com>
2025-08-05 15:54:54 +0300

    Create __init__.py

---
+++ pyastra/ports/pic14/procs/__init__.py
@@ -0,0 +1,57 @@
+############################################################################
+# $Id: __init__.py 95 2004-11-28 18:50:38Z estyler $
+#
+# Description: pic14 processors definitions. Pyastra project.
+# Author: Alex Ziranov <estyler _at_ users _dot_ sourceforge _dot_ net>
+#    
+# Copyright (c) 2004 Alex Ziranov.  All rights reserved.
+#
+############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+############################################################################
+
+__all__ = ['16c432', '16c433', '16c505', '16c554', '16c557', '16c558',
+           '16c52', '16c54', '16c55', '16c56', '16c57', '16c61',
+           '16c620a', '16c620', '16c621a', '16c621', '16c622a', '16c622',
+           '16c62a', '16c62b', '16c62', '16c63a', '16c63', '16c642',
+           '16c64a', '16c64', '16c65a', '16c65b', '16c65', '16c662',
+           '16c66', '16c67', '16c710', '16c711', '16c712', '16c715',
+           '16c716', '16c717', '16c71', '16c72a', '16c72', '16c73a',
+           '16c73b', '16c73', '16c745', '16c74a', '16c74b', '16c74',
+           '16c765', '16c76', '16c770', '16c771', '16c773', '16c774',
+           '16c77', '16c781', '16c782', '16c84', '16c923', '16c924',
+           '16c925', '16c926', '16ce623', '16ce624', '16ce625', '16cr62',
+           '16cr63', '16cr64', '16cr65', '16cr72', '16cr83', '16cr84',
+           '16f54', '16f57', '16f627a', '16f627', '16f628a', '16f628',
+           '16f630', '16f648a', '16f676', '16f684', '16f688', '16f716',
+           '16f72', '16f737', '16f73', '16f747', '16f74', '16f767',
+           '16f76', '16f777', '16f77', '16f818', '16f819', '16f83',
+           '16f84a', '16f84', '16f870', '16f871', '16f872', '16f873a',
+           '16f873', '16f874a', '16f874', '16f876a', '16f876', '16f877a',
+           '16f877', '16f87', '16f88', '16hv540', '14000', '12c671',
+           '12c672', '12ce518', '12ce519', '12ce673', '12ce674', '12f629',
+           '12c508a', '12c509a', '12c508', '12c509', '12f675', '12f683',
+           '12f508', '12f509', '12f635', '12f683', '16f505', '16f636',
+           '16f785', '12f629i', '16f630i', '16f737i', '16f870i', '16f874i',
+           '16f88i', '12f635i', '16f648ai', '16f747i', '16f871i',
+           '16f876ai', '12f675i', '16f676i', '16f767i', '16f872i',
+           '16f876i', '12f683i', '16f684i', '16f777i', '16f873ai',
+           '16f877ai', '16f627ai', '16f688i', '16f818i', '16f873i',
+           '16f877i', '16f628ai', '16f716i', '16f819i', '16f874ai',
+           '16f87i',
+           ]
+
