ff38e8bf827211996f4d4253f57ae0dba05ef68a
andrei1345 <72022571+andrei1345@users.noreply.github.com>
2025-08-05 14:18:11 +0300

    Update tree2tree.py

--- pyastra/tree2tree.py
+++ pyastra/tree2tree.py
@@ -2,28 +2,8 @@
 # $Id: tree2tree.py 95 2004-11-28 18:50:38Z estyler $
 #
 # Description: Port-independent optimizer. Pyastra project.
-# Author: Alex Ziranov <estyler _at_ users _dot_ sourceforge _dot_ net>
-#    
-# Copyright (c) 2004 Alex Ziranov.  All rights reserved.
-#
-############################################################################
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-#
-############################################################################
-#Author of original Pyastra
+# Author of original Pyastra: Alex Ziranov <estyler _at_ users _dot_ sourceforge _dot_ net>. See information at https://pyastra.sourceforge.net/
+
 import sys, os.path, compiler, pyastra.ports.pic14
 from ast import *
 from pyastra.ports.pic14.tree2asm import tree2asm
